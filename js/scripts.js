b3c3ca5ec8dadf1e0c47b206cb2a4eb106aa0c09
Phil Kendall <prkkendall@gmail.com>
2022-10-25 13:11:25 -0700

    create word counter functions, and working through filtering curse words.
    Co-authored-by: Aitana Shough <aeshough@gmail.com>

---
+++ js/scripts.js
@@ -0,0 +1,32 @@
+// Business Logic
+
+function wordCounter(text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  let wordCount = 0;
+  const textArray = text.split(" ");
+  textArray.forEach(function(element) {
+    if (!Number(element)) {
+      wordCount ++;
+    }
+  });
+  return wordCount;
+}
+
+function numberOfOccurrencesInText(word, text) {
+  const textArray = text.split(" ");
+  let wordCount = 0;
+  textArray.forEach(function(element) {
+    if (element.toLowerCase().includes(word.toLowerCase())) {
+      wordCount ++;
+    }
+  });
+  return wordCount;
+}
+
+function ommitCursedWords(text) {
+  if (text === zoinks) {
+    return "";
+  }
+}
